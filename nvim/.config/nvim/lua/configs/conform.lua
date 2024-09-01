38794b53fd5ea18bd95e74cb48eeb9e00ab8743e
barathdeva757@gmail.com <barathdeva757@gmail.com>
2024-09-01 19:02:02 +0530

    chore: update the format on save config in the neovim

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,15 +1,21 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    css = { "biome" },
+    html = { "biome" },
+    javascript = { "biome" },
+    javascriptreact = { "biome" },
+    typescript = { "biome" },
+    typescriptreact = { "biome" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = true,
+  },
 }
 
 return options
