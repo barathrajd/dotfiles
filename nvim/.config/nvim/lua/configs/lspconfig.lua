d528cb9c3450152cbbf1e2fef9558ee7c6a20cd6
barathdeva757@gmail.com <barathdeva757@gmail.com>
2024-09-07 16:23:45 +0530

    chore: config updated

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "biome", "tsserver", "denols", "rust_analyzer" }
+local servers = { "html", "cssls", "biome", "tsserver", "rust_analyzer", "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -3,8 +3,9 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+-- local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+-- map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
